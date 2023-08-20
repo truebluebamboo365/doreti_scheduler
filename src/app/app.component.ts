2c570c0331095b393a57af1af3b61c370e4418b6
truebluebamboo365 <51684345+truebluebamboo365@users.noreply.github.com>
2023-08-20 16:48:43 +0930

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxSchedulerModule, DxTemplateModule } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import { Service, Employee } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+})
+export class AppComponent {
+  dataSource: any;
+
+  currentDate: Date = new Date(2021, 5, 2, 11, 30);
+
+  resourcesDataSource: Employee[];
+
+  constructor(service: Service) {
+    this.dataSource = new DataSource({
+      store: service.getData(),
+    });
+
+    this.resourcesDataSource = service.getEmployees();
+  }
+
+  markWeekEnd(cellData) {
+    function isWeekEnd(date) {
+      const day = date.getDay();
+      return day === 0 || day === 6;
+    }
+    const classObject = {};
+    classObject[`employee-${cellData.groups.employeeID}`] = true;
+    classObject[`employee-weekend-${cellData.groups.employeeID}`] = isWeekEnd(cellData.startDate);
+    return classObject;
+  }
+
+  markTraining(cellData) {
+    const classObject = {
+      'day-cell': true,
+    };
+
+    classObject[AppComponent.getCurrentTraining(cellData.startDate.getDate(), cellData.groups.employeeID)] = true;
+    return classObject;
+  }
+
+  static getCurrentTraining(date, employeeID) {
+    const result = (date + employeeID) % 3;
+    const currentTraining = `training-background-${result}`;
+
+    return currentTraining;
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserTransferStateModule,
+    DxSchedulerModule,
+    DxTemplateModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
